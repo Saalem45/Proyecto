9efb52fb44ac81aefac34b8af7d0dc3e390393ac
Saalem45 <167729100+Saalem45@users.noreply.github.com>
2024-12-20 17:43:24 -0600

    Add files via upload

---
+++ dataBase/config.js
@@ -0,0 +1,14 @@
+//Importar las variables de entorno
+import { config } from "dotenv"
+config('../.env')
+
+const MyConfig = {
+    mysql: {
+        host: process.env.MYSQL_HOST || 'localhost',
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASSWORD || '12345',
+        database: process.env.MYSQL_DB || 'itj_Escolares'
+    }
+}
+
+export default MyConfig
